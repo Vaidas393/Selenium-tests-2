86678c9acfbfd3fba799b94624e150f1d584699a
Vaidas393 <37333650+Vaidas393@users.noreply.github.com>
2021-05-05 00:00:15 +0300

    Update Generator.java

--- src/main/java/utils/Generator.java
+++ src/main/java/utils/Generator.java
@@ -30,7 +30,7 @@ public class Generator {
 
 			public static void main(String[] args)
 			{
-//			    That characters you want to use in password generator
+//			    What characters you want to use in password generator
 			    char[] set1 = {'a', 'A', 'b', 'B', 'c', 'C','d', 'D', 'e', 'E', 'f', 'F', 'g', 'G', 'h', 'H', 'i', 'I', 'j', 'J', 'k', 'K', 'l', 'L', 'm', 'M', 'n', 'N', 'o', 'O', 'p', 'P', 'q', 'Q', 'r', 'R', 's', 'S', 't', 'T', 'u', 'U', 'v', 'V', 'w', 'W', 'x', 'X', 'y', 'Y', 'w', 'W', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'  };
 
 
